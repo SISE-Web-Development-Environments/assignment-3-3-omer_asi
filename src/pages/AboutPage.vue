ac856e4857a421d3b734d3691333fa6ff8226222
Nagar, Omer <omer.nagar@intel.com>
2020-07-09 17:23:47 +0300

    search functionality

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -1,5 +1,13 @@
 <template>
   <div class="container">
     <h1 class="title">About Page</h1>
-  </div>
+    <p>Created By Asaf Zaks and Omer Nagar</p>
+    <p>Search, favorite, and cerate records for some of your own recipes.</p>
+    <p>Some of our recent projects:</p>
+    <a href="https://sise-web-development-environments.github.io/assignment1-AsiZaks/">Asi's personal page</a>
+    <br>
+    <a href="https://sise-web-development-environments.github.io/assignment1-omernag/">Omer's personal page</a>
+    <br>
+    <a href="https://sise-web-development-environments.github.io/assignment2-asi-omer/">Corona Packman game</a>
+    </div>
 </template>
